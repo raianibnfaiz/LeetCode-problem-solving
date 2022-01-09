8eed69f266fa54a72f5a1b04e3bf0d73dba2ed23
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2022-01-09 13:17:59 +0600

    third commit

--- max_distance_in_array.py
+++ max_distance_in_array.py
@@ -1,11 +1,24 @@
-a=[10,9,12,1]
-numbers=[]
-for i,b in enumerate(a):
-    numbers.append((b,i))
+#given a list of non-negative integers ,find the maximum distance j-i , where A[i]<=A[j]
+#array[4,2,8,1,0,7] =>A[4]=0 A[3]=8 i=4 j=3 j-i=3-4=-1  sort[0,1,2,4,7,8] 
+#[(4,0),(2,1),(8,2),(1,3),(0,4),(7,5)]
+#sorted_array[(0,4),(1,3),(2,1),(4,0),(7,5)(8,2)]
+#minimum_index_number=4
+#find maximum j-i
+#https://www.interviewbit.com/problems/max-distance/
 
-numbers.sort()
-print(numbers)
-minNumber=numbers[2]
-print(numbers[3][1])
-for items in numbers:
-    print(items)
+def maximumDistance(A):
+    numbers=[]
+    for i,num in enumerate(A):
+        numbers.append((num,i))
+    numbers.sort()
+    max_gap=0
+    minimum_index_number=numbers[0][1]
+    for element in numbers:
+        index_number=element[1]
+        if index_number<=minimum_index_number:
+            minimum_index_number=index_number
+        else:
+            max_gap=max(max_gap, index_number-minimum_index_number)
+    return max_gap
+A=[4,2,8,1,0,7]
+print(maximumDistance(A))
